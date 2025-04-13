83aabc49b3e39f5cf6400abfc0c931e38102dd7d
Rubaya Akter Mim <45107866+rubayamim381@users.noreply.github.com>
2025-04-13 12:48:19 +0600

    Add [1922. Count Good Numbers] : Describe the approach

--- 1922.cpp
+++ 1922.cpp
@@ -1,3 +1,10 @@
+// If digits are in even position, choose from 5 options (even digits) : 0,2,4,6,8
+// If digits are in odd position, choose from 4 options (Prime digits) : 2,3,5,7
+// Total Good Numbers of length n: = 5^(n_even_positions) × 4^(n_odd_positions)
+
+// TC : O(logn)
+// SC : O(1)
+
 class Solution {
 public:
     const int MOD = 1e9 + 7;
@@ -5,8 +12,7 @@ public:
     int countGoodNumbers(long long n) {
         long long evenPosLength = (n + 1) / 2;
         long long oddPosLength = n / 2;
-
-        // 5^(n_even_positions) × 4^(n_odd_positions)
+        
         long long evenWays = countPower(5, evenPosLength);
         long long oddWays = countPower(4, oddPosLength);
         
